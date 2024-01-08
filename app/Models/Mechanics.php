1894d33eae8582f69c2549a17692551c713c9150
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2024-01-08 18:05:44 +0600

    has one through

---
+++ app/Models/Mechanics.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mechanics extends Model
+{
+    use HasFactory;
+    public function carOwner()
+    {
+        return $this->hasOneThrough(Owners::class, Cars::class);
+    }
+}
---
+++ database/migrations/2024_01_08_113733_create_cars__table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Mechanics;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->integer('mechanics_id');
+            $table->text('model');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+};
